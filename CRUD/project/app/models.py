6ba532a660c4e2a773e6c8b6ce0d7e0a3104a313
Your Bhagibhagya <koruprolubhagyaraju@gmail.com>
2025-01-22 18:52:43 +0530

    update

--- CRUD/project/app/models.py
+++ CRUD/project/app/models.py
@@ -17,6 +17,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'product'
+
     def __str__(self):
         return self.name
 
